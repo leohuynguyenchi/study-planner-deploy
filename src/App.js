d82cbb93cd78b14b37eb913211744c4a50c00e6e
leohuynguyenchi <92219545+leohuynguyenchi@users.noreply.github.com>
2024-02-02 14:45:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import DevPlan from "./components/Development Plan/dev-plan";
+
+function App() {
+  return (
+  <div className="App">
+    <div className="m-12">hello</div>
+    <DevPlan />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Development Plan/dev-plan.js
@@ -0,0 +1,28 @@
+import "./dev-plan.css";
+
+function DevPlan() {
+    return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Nhiệm Vụ</th>
+                <th>Người được giao việc</th>
+                <th>Deadline</th>
+                <th>Trạng thái</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>New 1</td>
+                <td>Leo</td>
+                <td>12-1</td>
+                <td>In Progress</td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+    );
+}
+
+export default DevPlan;
